d4e625324cfbc2adedc8db05fff5f6dd938f2642
Hovakim Grabski <hovakim_grabski@yahoo.com>
2019-09-25 18:36:02 +0400

    My custom take on this project start

--- Python/blocker.py
+++ Python/blocker.py
@@ -13,29 +13,40 @@ else:
     print('\tDetected Platform was',platform.system())
 
 redirect_path = "127.0.0.1"
-websites = ["www.facebook.com", "facebook.com", "twitter.com", "www.twitter.com"]
+websites = ["www.facebook.com",
+ "facebook.com", "twitter.com", "www.twitter.com",
+ " youtube.com", "www.youtube.com"]
+
+
+# Need to make script very adaptable
 
 # To make the script running all the time
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 18) < dt.now() <  dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print("Time to block website!")
-        # Open the hosts file
-        with open(host_file_path, 'r+') as file:
-            # Read the content and print it out on the terminal
-            content = file.read()
-            print(content)
-            for website in websites:
-                if website in content:
-                    pass
-                else:
-                    file.write (redirect_path + " " + website + "\n")
-    else:
-        with open (host_file_path, 'r+') as file:
-            content = file.readlines()
-            file.seek(0)
-            for line in content:
-                if not any(website in line for website in websites):
-                    file.write(line)
-            file.truncate()
-        print("Websites are unblocked!")
+    time1 = dt(dt.now().year, dt.now().month, dt.now().day, 18)
+    time2 = dt(dt.now().year, dt.now().month, dt.now().day, 16)
+    print('time 1 is ', time1)
+    print('time 2 is ', time2)
     time.sleep(10)
+    #if time1 <  time2:
+        
+    #     print("Time to block website!")
+    #     # Open the hosts file
+    #     with open(host_file_path, 'r+') as file:
+    #         # Read the content and print it out on the terminal
+    #         content = file.read()
+    #         print(content)
+    #         for website in websites:
+    #             if website in content:
+    #                 pass
+    #             else:
+    #                 file.write (redirect_path + " " + website + "\n")
+    # else:
+    #     with open (host_file_path, 'r+') as file:
+    #         content = file.readlines()
+    #         file.seek(0)
+    #         for line in content:
+    #             if not any(website in line for website in websites):
+    #                 file.write(line)
+    #         file.truncate()
+    #     print("Websites are unblocked!")
+    # time.sleep(10)
